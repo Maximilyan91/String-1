0536dc3a06cea5cc6b671754a21d4ac75ee4116a
Максим <gmax7911@gmail.com>
2022-12-18 11:25:20 +0300

    Задача 1 и 2 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
         public static void task1() {
             String firstName = "Ivan";
@@ -9,4 +10,10 @@ public class Main {
             String fullName = middleName + " " + firstName + " " + lastName;
             System.out.println("ФИО сотрудника - " + fullName);
         }
+        public static void task2() {
+            String fullName = "Ivanov Ivan Ivanovich";
+            System.out.println(fullName.toUpperCase());
+        }
+
+
     }
